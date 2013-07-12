4951adbf1c2e0fb5bbb589e516e4312a4d458de7
AnandR <r_anand98@outlook.com>
2013-07-12 06:07:23 +0000

    reading from file

--- web.js
+++ web.js
@@ -2,8 +2,17 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var file = require('fs');
+
+var str = file.readFileSync("index.html");
+
+var output = new Buffer(256);
+
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  
+    response.send(output.toString());
 });
 
 var port = process.env.PORT || 5000;
